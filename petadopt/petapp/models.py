a9322d3fcd2e14dc4a5b5cb4a847b6257d8a721f
Jenaleigh <jenaleighfox@gmail.com>
2020-03-23 14:11:27 -0700

    updated  models

--- petadopt/petapp/models.py
+++ petadopt/petapp/models.py
@@ -4,15 +4,15 @@ from django.db import models
 
 class Pet(models.Model):
     SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
-    name = models.CharField(max_length=100)
+    petName = models.CharField(max_length=100)
     submitter = models.CharField(max_length=100)
-    species = models.CharField(max_length=30)
-    breed = models.CharField(max_length=30, blank=True)
-    description = models.TextField()
-    sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
     submission_date = models.DateTimeField()
-    age = models.IntegerField(null=True)
-    vaccinations = models.ManyToManyField('Vaccine', blank=True)
+    petSpecies = models.CharField(max_length=30)
+    petBreed = models.CharField(max_length=30, blank=True)
+    petDescription = models.TextField()
+    petSex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
+    petAge = models.IntegerField(null=True)
+    petVaccinations = models.ManyToManyField('Vaccine', blank=True)
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
